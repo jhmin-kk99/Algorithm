684643545587ec1b9b49df595ddfa72909c3f38f
Jihong Min <jihongmin99@naver.com>
2023-04-27 21:56:04 +0900

    9095

--- DP/9095.cpp
+++ DP/9095.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
 int d[12];
-int n, t;
+int t;
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cin >> t;
+    d[1] = 1; d[2] = 2; d[3] = 4;
+    for (int i = 4; i <= n; i++)
+        d[i] = d[i - 1] + d[i - 2] + d[i - 3];
     while (t--)
     {
+        int n;
         cin >> n;
-        d[1] = 1;
-        d[2] = 2;
-        d[3] = 4;
-
-        for (int i = 4; i <= n; i++)
-        {
-            d[i] = d[i - 1] + d[i - 2] + d[i - 3];
-        }
         cout << d[n] << '\n';
     }
 
