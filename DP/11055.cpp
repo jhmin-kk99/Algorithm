5afb3adc9489ca3c891e0ce7c14cc2a7896e61b6
Jihong Min <jihongmin99@naver.com>
2023-05-02 02:07:04 +0900

    11055

---
+++ DP/11055.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int n;
+int d[1005], f[1005];
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> f[i];
+        d[i] = f[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (f[i] > f[j])
+            {
+                d[i] = max(d[i], d[j] + f[i]); // ex) 1 49 25 26 27 28 50
+            }
+        }
+    }
+    cout << *max_element(d, d + n);
+}
+
+
+
+//1 <= N <= 1000 이므로 O(N^2)도 고려해볼 수 있음
+//D[i] = i까지의 수열에서 f[i]가 가장 클 때의 증가하는 부분 수열의 최대 합
+
+
+
